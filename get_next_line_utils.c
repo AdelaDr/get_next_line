7f0376b78fd1455f1e8071c5914e4495866eb59d
AdelaDr <drahotovaadela@seznam.cz>
2026-01-13 18:39:25 +0100

    strchr ft

---
+++ get_next_line_utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 22:12:34 by adrahoto          #+#    #+#             */
+/*   Updated: 2026/01/08 22:12:34 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if (c == '\0')
+		return ((char *)str);
+	return (NULL);
+}
