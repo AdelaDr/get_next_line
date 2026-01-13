d41e992b951de2816bab530e73a4aeb2b2f04594
AdelaDr <drahotovaadela@seznam.cz>
2026-01-13 18:41:13 +0100

    strchr + header

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrahoto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 22:12:34 by adrahoto          #+#    #+#             */
+/*   Updated: 2026/01/08 22:12:34 by adrahoto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
 char *get_next_line(int fd);
+char	*ft_strchr(const char *str, int c);
 
 #endif
