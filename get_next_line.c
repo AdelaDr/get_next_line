4920b15b85ba4b2d687444f853d5433acb2051ab
AdelaDr <drahotovaadela@seznam.cz>
2026-01-14 23:18:07 +0100

    variables + read

--- get_next_line.c
+++ get_next_line.c
@@ -9,3 +9,21 @@
 /*   Updated: 2026/01/08 22:12:34 by adrahoto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    static char stash;
+    char buffer[BUFFER_SIZE + 1];
+    char *newline;
+    int bytes;
+
+    while (newline == NULL)
+    {
+        bytes = read(fd, buffer, BUFFER_SIZE);
+        if (bytes <= 0)
+            break;
+    }
+}
